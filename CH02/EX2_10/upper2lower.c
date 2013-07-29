39fad7334fd044ffe9e80210d997c717a2b73a0d
skynjupt <skynjupt@gmail.com>
2013-07-29 13:39:34 +0800

    add the title

--- CH02/EX2_10/upper2lower.c
+++ CH02/EX2_10/upper2lower.c
@@ -1,3 +1,6 @@
+/*
+ * Exercise 2-10. Rewrite the function lower, which converts upper case letters to lower case, with a conditional expression instead of if-else.
+ */ 
 #include <stdio.h> 
 #include <string.h>
 
